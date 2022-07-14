2f0fddb0a93d5abf5bf33980445d8754cdb13a0d
Enrqiue <jesus.tun.mx@cloudappi.net>
2022-07-13 23:34:26 -0500

    feat(redux): redux has been added to the request for the screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import { Provider } from 'react-redux';
+import type { AppProps } from 'next/app';
+import  { store } from '../store/store';
+
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ 
+  Component, pageProps 
+}: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
